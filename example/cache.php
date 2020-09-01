dbc96bf1a9644b02b87876bd98346760bcaa104e
jgauthi <jgauthi@users.noreply.github.com>
2020-09-01 19:17:56 +0200

    [Archive] 30 aout 2011: php 5 version + PSR-4 Autoloader

---
+++ example/cache.php
@@ -0,0 +1,53 @@
+<?php
+// In this example, the vendor folder is located in "example/"
+use Jgauthi\Component\Cache\Cache;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+//-- Configuration ----------------------
+$url = 'http://fr.wikipedia.org/wiki/Bombardements_atomiques_de_Hiroshima_et_Nagasaki';
+$debut = '<a href="/wiki/Fichier:Atomic_cloud_over_Hiroshima.jpg"';
+$fin = '<table class="plainlinks" id="article_de_qualite"';
+define('TMP_PATH', sys_get_temp_dir()); // You can set your tmp folder
+
+//---------------------------------------
+
+
+$data = new Cache($url, '5i', TMP_PATH); // 5min
+
+if ($data->existe()) {
+    $contenu = $data->contenu();
+
+} else {
+    $stripBody = function ($html) {
+        if (preg_match("/<body[^>]*>(.*?)<\/body>/is", $html, $row)) {
+            return $row[1];
+        }
+
+        return $html;
+    };
+
+    $page = file_get_contents($url);
+    $page = preg_replace("#^(.+){$debut}#i", $debut, $page);
+    $page = preg_replace("#{$fin}(.+)$#i", '', $page);
+    $page = preg_replace('#src="/#Di', 'src="http://fr.wikipedia.org/', $page);
+    $page = $stripBody($page);
+    $contenu = $data->contenu($page);
+}
+
+?>
+<style type="text/css">
+    .contenu
+    {
+        width: 75%;
+        margin-left: auto;
+        margin-right: auto;
+        padding: 15px;
+        border: 1px dashed black;
+        background-color: #ccc;
+        font-size: 0.9em;
+    }
+</style>
+
+<h1>Téléchargement d'une page et mise en cache</h1>
+<div class="contenu"><?=$contenu?></div>
--- src/Cache.php
+++ src/Cache.php
@@ -1,187 +1,236 @@
 <?php
-/**
- * Cache - Data storage in the form of temporary files (cache)
- * @author Jgauthi <github.com/jgauthi>
- */
+/*******************************************************************************
+ * @name: Cache
+ * @note: Data storage in the form of temporary files (cache)
+ * @author: Jgauthi <github.com/jgauthi>, created at [14mars2009]
+ * @version 1.1
+
+ *******************************************************************************/
+
+namespace Jgauthi\Component\Cache;
+
+use InvalidArgumentException;
 
 class Cache
 {
-    // Class var
-    var $nom;
-    var $date;
-    var $fichier;
-    var $contenu;
-    var $etat;
-    var $dir;
+    public $nom;
+    public $dir;
+    public $date;
+    protected $fichier;
+    protected $contenu;
+    protected $type;
+    protected $etat = null;
 
-    /*-- CONSTRUCTEUR -----------------------------------------------------*/
-    FUNCTION cache($nom, $date, $dossier = 'tmp/', $type = 'tpl')
+    /**
+     * cache constructor.
+     *
+     * @param string $nom
+     * @param string $date
+     * @param string $dossier
+     * @param string $type
+     */
+    public function __construct($nom, $date, $dossier = 'tmp', $type = 'tpl')
     {
         // Type de cache
-        if 	(eregi("^dat",$type)) 			{	$this -> type = 'data';		}
-        elseif (eregi("(html|tpl)",$type))	{	$this -> type = 'template';	}
-        elseif (eregi("(sql|db)",$type))	{	$this -> type = 'sql';		}
-        else								{	$this -> type = 'file';		}
-
-        // Configuration dossier temporaire
-        if (!file_exists($dossier) || !is_dir($dossier))
-        {
-            // Tentative de créer le dossier
-            if (!mkdir($dossier, 0777))
-            {
-                user_error(	'Impossible de créer le dossier "'. $dossier .
-                    '", la class cache ne peut pas fonctionner.');
-                return (die());
-            }
+        if (preg_match('#^dat#i', $type)) {
+            $this->type = 'data';
+        } elseif (preg_match('#(html|tpl)#i', $type)) {
+            $this->type = 'template';
+        } elseif (preg_match('#(sql|db)#i', $type)) {
+            $this->type = 'sql';
+        } else {
+            $this->type = 'file';
         }
-        $this -> dir = $dossier;
+
+        // Tentative de créer le dossier
+        if (!is_dir($dossier) && !mkdir($dossier, 0777)) {
+            throw new InvalidArgumentException("Impossible de créer le dossier '$dossier', la class ".__CLASS__.' ne peut pas fonctionner');
+        }
+
+        $this->dir = $dossier.'/';
 
         // Info du cache
-        $this -> nom = md5($nom . 'AllanTK @ PHP');
-        $this -> date = $this -> _date($date);
-        $this -> fichier = $this -> dir.'cache_'.$this -> nom . $this -> _extension($this -> type);
-
-        // Stocker la liste des caches utilisés si DEBUG
-        //$_LOG['cache'][] = $this -> nom.$ext; // Fonction Akila
+        $this->nom = md5($nom.$_SERVER['HTTP_HOST']);
+        $this->date = $this->_date($date);
+        $this->fichier = $this->dir.'cache_'.$this->nom.$this->_extension($this->type);
     }
 
     //-- FONCTIONS ----------------------------------------------------------
-    FUNCTION existe()
+
+    /**
+     * @return bool|null
+     */
+    public function existe()
     {
-        // Mini cache
-        if (!empty($this -> etat))
-        {
-            if ($this -> etat == 'ok')	return TRUE;
-            else						return FALSE;
-            exit();
+        // Si le cache existe
+        if (null === $this->etat) {
+            $this->etat = false;
+
+            if (is_readable($this->fichier)) {
+                if (@filemtime($this->fichier) > (time() - $this->date)) {
+                    $this->etat = true;
+                } else {
+                    $this->detruire();
+                }
+            }
         }
 
-        // Si le cache existe
-        if (@is_file($this -> fichier))
-        {
-            if (@filemtime($this -> fichier) > time()- $this -> date)
-            {
-                $this -> etat = 'ok';
-                return TRUE;
-                exit();
-            }
-            else
-            {
-                $this -> detruire();
-                $this -> etat = 'non';
-            }
-        }
-        else	$this -> etat = 'non';
-        return FALSE;
+        return $this->etat;
     }
 
-    // Gestionnaire de contenu
-    FUNCTION contenu($contenu = '')
+    /**
+     * Gestionnaire de contenu.
+     *
+     * @param string $contenu
+     *
+     * @return array|false|mixed|string
+     */
+    public function contenu($contenu = '')
     {
-        if (($this -> type == 'data' || $this -> type == 'sql') && (!is_array($contenu) && !is_object($contenu)))
-            $this -> contenu = unserialize(file_get_contents($this -> fichier));
-        elseif ($this -> type == 'template' && $this -> etat == 'ok')
-            $this -> contenu = file_get_contents($this -> fichier);
-        elseif ($this -> type == 'template' && !empty($contenu))
-            $this -> contenu = $contenu;
-        /*		elseif ($this -> type == 'template' && is_object($site)) // vieille condition avec $site (akila)
-                                                $this -> contenu = $site -> OutPut();*/
-        elseif (!empty($contenu))		$this -> contenu = $contenu;
+        if (('data' === $this->type || 'sql' === $this->type) && (!is_array($contenu) && !is_object($contenu))) {
+            $this->contenu = unserialize(file_get_contents($this->fichier));
+        } elseif ('template' === $this->type && $this->etat) {
+            $this->contenu = file_get_contents($this->fichier);
+        } elseif ('template' === $this->type && !empty($contenu)) {
+            $this->contenu = $contenu;
+        } elseif (!empty($contenu)) {
+            $this->contenu = $contenu;
+        }
 
         // Envoyer le contenu si $contenu = TRUE
-        return ($this -> contenu);
+        return $this->contenu;
     }
 
-    // Création du cache en indiquant une valeur si contenu() n'a pas été utilisé
-    FUNCTION creer($contenu = '')
+    /**
+     * Création du cache en indiquant une valeur si contenu() n'a pas été utilisé.
+     *
+     * @param string $contenu
+     *
+     * @return bool
+     */
+    public function creer($contenu = '')
     {
-        if (empty($contenu))
-        {
-            if (empty($this -> contenu))
-                return (user_error('Impossible de créer le cache, pas de contenu'));
-            else	$contenu = $this -> contenu;
+        if (empty($contenu)) {
+            if (empty($this->contenu)) {
+                throw new InvalidArgumentException('Impossible de créer le cache, pas de contenu');
+            }
+            $contenu = $this->contenu;
         }
 
         // Signature
-        switch($this -> type)
-        {
+        switch ($this->type) {
             case 'template':
-                $contenu .= "\n\n".'<!-- ATK-CACHE {'.$this -> nom.'} time: '.date("d-m-Y, H:i:s").' -->'."\n\n";
+                $contenu .= "\n\n".'<!-- CACHE {'.$this->nom.'} time: '.date('d-m-Y, H:i:s').' -->'."\n\n";
                 break;
 
-
-            case 'data': case 'sql' :
-            $contenu = serialize($contenu);
-            break;
-
+            case 'data': case 'sql':
+                $contenu = serialize($contenu);
+                break;
 
             default:
-                return (user_error('Donné du Cache ['.$this -> type.'] id: {'.$this -> nom.'} corrompu'));
-                break;
+                throw new InvalidArgumentException('Donné du Cache ['.$this->type.'] id: {'.$this->nom.'} corrompu');
         }
 
         // Création du cache
-        $fichier = @fopen($this -> fichier,'w');
-        fputs($fichier,$contenu);
+        $fichier = @fopen($this->fichier, 'w');
+        fwrite($fichier, $contenu);
         fclose($fichier);
+
+        return file_exists($this->fichier);
     }
 
-    // Effacer un cache
-    FUNCTION detruire ($fichier = 'self', $type = 'tpl')
+    /**
+     * Effacer un cache.
+     *
+     * @param null   $fichier
+     * @param string $type
+     *
+     * @return bool
+     */
+    public function detruire($fichier = null, $type = 'tpl')
     {
-        if ($fichier == 'self')
-            $fichier = $this -> fichier;
-        else	$fichier = $this -> dir.md5($fichier) . $this -> _extension($type);
+        if (null === $fichier) {
+            $fichier = $this->fichier;
+        } else {
+            $fichier = $this->dir.md5($fichier).$this->_extension($type);
+        }
 
-        @unlink($fichier);
+        return @unlink($fichier);
     }
 
-    // Clean cache (supprimer tous les caches crée par cette class)
-    function effacer_cache($dossier = 'tmp/')
+    /**
+     * Clean cache (supprimer tous les caches crée par cette class).
+     *
+     * @param string|null $dossier
+     *
+     * @return bool|int
+     */
+    public function effacer_cache($dossier = null)
     {
-        if (empty($dossier))
-            $dossier = $this -> dir;
+        if (null === $dossier) {
+            $dossier = $this->dir;
+        }
 
         // Vérifier le dossier
-        if (file_exists($dossier) && is_dir($dossier))
-        {
+        if (is_dir($dossier)) {
             $dir = opendir($dossier);
-            while ($file = readdir($dir))
-            {
-                $ext = $this -> _extension($this -> type);
+            $nb_file_delete = 0;
+            while ($file = readdir($dir)) {
+                if ('.' === $file && '..' === $file) {
+                    continue;
+                }
 
-                // Effacer les fichiers cache
-                if ($file != "." && $file != ".." && eregi("^cache_([a-f0-9]{32})\.".$ext."$", $file))
-                    @unlink($file);
+                $ext = $this->_extension($this->type);
+
+                // Effacer les fichiers caches
+                if (preg_match("#^cache_([a-f0-9]{32})\.".$ext.'$#i', $file) && @unlink($file)) {
+                    ++$nb_file_delete;
+                }
             }
             closedir($dir);
+
+            return $nb_file_delete;
         }
-        else	return (false);
+
+        return false;
     }
 
-    //-- FONCTIONS SPECIFIQUE -------------------------------------------------
-
     //-- Sous fonctions -------------------------------------------------------
-    FUNCTION _date($date)
+
+    /**
+     * @param string $date
+     *
+     * @return mixed
+     */
+    protected function _date($date)
     {
-        if (eregi("[ihjsmac]",$date))
-        {
-            $date = str_replace(array('i','h','j','s','m','a','c'),
-                array('*60','*3600','*86400','*604800','*2419200','*29030400','*1451520000'),$date);
+        if (preg_match('#[ihjsmac]#i', $date)) {
+            $date = str_replace(['i', 'h', 'j', 's', 'm', 'a', 'c'],
+                ['*60', '*3600', '*86400', '*604800', '*2419200', '*29030400', '*1451520000'], $date);
 
             eval('$date = '.$date.';');
         }
+
         return $date;
     }
 
-    function _extension($type)
+    /**
+     * @param string $type
+     *
+     * @return string
+     */
+    protected function _extension($type)
     {
-        // Type de cache
-        if 	(eregi("^dat",$type)) 			$ext = '.dat';
-        elseif (eregi("(html|tpl)",$type))	$ext = '.tpl';
-        elseif (eregi("(sql|db)",$type))	$ext = '.sql';
-        else								$ext = '.tmp';
+        if (preg_match('#^dat#i', $type)) {
+            $ext = '.dat';
+        } elseif (preg_match('#(html|tpl)#i', $type)) {
+            $ext = '.tpl';
+        } elseif (preg_match('#(sql|db)#i', $type)) {
+            $ext = '.sql';
+        } else {
+            $ext = '.tmp';
+        }
 
-        return ($ext);
+        return $ext;
     }
 }
